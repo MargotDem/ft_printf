2b65ef2dc756a30796ed8c50b744a57ca22a0e2c
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2022-03-26 16:15:44 +0200

    refacto conv u, norm

---
+++ conv_u.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   conv_u.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mde-maul <mde-maul@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/26 16:14:42 by mde-maul          #+#    #+#             */
+/*   Updated: 2022/03/26 16:14:44 by mde-maul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	handle_decimal(t_options *options, va_list *list, size_t *char_count)
+{
+	char					*nb_str;
+	unsigned long long int	nb;
+	size_t					len;
+
+	set_nb_unsigned(options, list, &nb);
+	nb_str = ft_ull_itoa_base(nb, 10);
+	len = ft_strlen(nb_str);
+	print_out_nb_str(options, char_count, len, nb_str);
+}
--- utils.c
+++ utils.c
@@ -154,15 +154,3 @@ void	set_nb_unsigned(t_options *options, va_list *list, unsigned long long int *
 	else
 		*nb = va_arg(*list, unsigned int);
 }
-
-void	handle_decimal(t_options *options, va_list *list, size_t *char_count)
-{
-	char					*nb_str;
-	unsigned long long int	nb;
-	size_t					len;
-
-	set_nb_unsigned(options, list, &nb);
-	nb_str = ft_ull_itoa_base(nb, 10);
-	len = ft_strlen(nb_str);
-	print_out_nb_str(options, char_count, len, nb_str);
-}
